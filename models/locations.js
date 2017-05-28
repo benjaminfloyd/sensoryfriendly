5350e68c5decfa29efeb118527edd67d09b7adf7
benjaminfloyd <benjamin.floyd@me.com>
2017-05-28 09:50:43 -0400

    locations added to schema

--- models/locations.js
+++ models/locations.js
@@ -1,6 +1,27 @@
-lines (4 sloc)  122 Bytes
-var Schema = require("../db/schema");
-var mongoose = require('mongoose');
 
-var Locations = Schema.Locations;
-module.exports = LocationsSchema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// mongoose.promise = global.Promise;
+
+var LocationsSchema = new Schema({
+  business_name: String,
+  phone_Number: String,
+  web_address: String,
+//   type:[{}]
+});
+
+LocationsSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+
+LocationsSchema.virtual('fullName').get(function () {
+  return this.first_name + ' ' + this.last_name;
+});
+
+module.exports = mongoose.model("Locations", LocationsSchema);.exports = LocationsSchema;
